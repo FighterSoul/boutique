f7a1b99d552f818513757a46cfe4376659982be1
FighterSoul <hedi.abdessalem@gmail.com>
2023-10-24 20:36:50 +0200

    first commit

---
+++ boutique/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientModule } from './client/client.module';
+import { AdminModule } from './admin/admin.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ClientModule,
+    AdminModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
