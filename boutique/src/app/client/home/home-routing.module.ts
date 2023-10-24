e4c0a06668e5aa9defefb28401fe4cb3dfb0a894
FighterSoul <hedi.abdessalem@gmail.com>
2023-10-24 21:04:26 +0200

    finished tp1 and tp2

--- boutique/src/app/client/home/home-routing.module.ts
+++ boutique/src/app/client/home/home-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeadmComponent } from 'src/app/admin/homeadm/homeadm.component';
+import { HomeComponent } from 'src/app/client/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- boutique/src/app/client/home/home.module.ts
+++ boutique/src/app/client/home/home.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { HomeProductDetailComponent } from './home-product-detail/home-product-detail.component';
+import { HomeProductsComponent } from './home-products/home-products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeBannerComponent,
+    HomeProductDetailComponent,
+    HomeProductsComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
   ]
 })
 export class HomeModule { }
+export class HomeComponent{ }
